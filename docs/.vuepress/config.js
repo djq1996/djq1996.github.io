0eff7f1d15edec366d1e5186e187518231340a64
dongjianqiang <dongjianqiang@ann9.com>
2018-11-28 19:46:00 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'JianQiang blog',
   description: '种一棵树，最好的时间是十年前，其次是现在。',
   port: 1996,
-  base: '/starry/',
+  // base: '/starry/',
   dest: 'starry',
   head: [
     [
